ecaaf5ab33da2a7d5f82bfeec615fd40acc664cc
AnatoliyKotok <61416716+AnatoliyKotok@users.noreply.github.com>
2020-11-19 17:19:16 +0200

    Add files via upload

---
+++ AppSettingHelper/AppSettingHelper/AppSettingHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettingHelper
+{
+    class AppSettingHelper
+    {
+        public static void WriteSetting(string fname)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fname, FileMode.OpenOrCreate)))
+            {
+                
+                writer.Write(Console.BackgroundColor.ToString());
+                writer.Write(Console.Title);
+                writer.Write(Console.WindowWidth);
+                writer.Write(Console.WindowHeight);
+            }
+        }
+        public static void ReadSetting(string fname)
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(fname, FileMode.Open)))
+            {
+                
+                while (reader.PeekChar() > -1)
+                {
+                    string color= reader.ReadString();
+                    string titel = reader.ReadString();
+                    int width = reader.ReadInt32();
+                    int height = reader.ReadInt32();
+
+                    Console.WriteLine($"Color:{color}\nTite:{titel}\nWidth:{width}\nHeight:{height}");
+                       
+                }
+            }
+        }
+    }
+}
---
+++ AppSettingHelper/AppSettingHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettingHelper_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname = "Setting.txt";
+            AppSettingHelper.AppSettingHelper.WriteSetting(fname);
+            AppSettingHelper.AppSettingHelper.ReadSetting(fname);
+        }
+    }
+}
